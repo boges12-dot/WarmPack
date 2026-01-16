fc4c80bcc1b6acd37139e95ae5f76c0d1cf5aacb
boges12-dot <bongkion1004@gmail.com>
2026-01-16 15:26:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -123,7 +123,7 @@ function initUlNavDropdown(){
     try { canHover = window.matchMedia && window.matchMedia('(hover: hover) and (pointer: fine)').matches; } catch(e) {}
     if(!canHover) return;
 
-    var CLOSE_DELAY_MS = 1200;
+    var CLOSE_DELAY_MS = 900;
     nav.querySelectorAll('li.has-sub').forEach(function(li){
       var t = null;
 
