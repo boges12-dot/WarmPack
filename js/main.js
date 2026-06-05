dd36dc31876611d6fd670753abad4aca3b8231c5
Wi Bongkion <bongkion1004@gmail.com>
2026-06-05 14:09:01 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-document.querySelector('.menu-toggle')?.addEventListener('click',()=>document.querySelector('.nav').classList.toggle('open'));
+const btn = document.querySelector('.mobile-btn');
+const menu = document.querySelector('.menu');
+btn?.addEventListener('click', () => {
+  menu?.classList.toggle('open');
+  btn.classList.toggle('open');
+});
